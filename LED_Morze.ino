14fd3273fac62724d3d50c98d93e4f205ec849ea
Ruslan Semybalamut <ruslansemybalamut@gmail.com>
2016-04-26 00:11:10 +0300

    2

---
+++ LED_Morze.ino
@@ -0,0 +1,15 @@
+#define LED_A 13
+void setup() {
+ pinMode(LED_A, OUTPUT);
+
+void LED_ Semubalamyt ( ) {
+ digitalWrite (LED_A, HIGH);
+ delay (500);
+ digitalWrite (LED_A, LOW);
+ delay (500);
+
+ digitalWrite (LED_A, HIGH);
+ delay (500);
+ digitalWrite (LED_A, LOW);
+ delay (500);
+}
